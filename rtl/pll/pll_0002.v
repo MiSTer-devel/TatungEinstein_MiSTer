9451b63d3c9ea760a1b74cd3a40d724f99628dd5
pierco <pier.cornier@gmail.com>
2022-01-10 11:02:31 +0100

    fixed timing

--- rtl/pll/pll_0002.v
+++ rtl/pll/pll_0002.v
@@ -25,7 +25,7 @@ module  pll_0002(
 		.output_clock_frequency0("32.000000 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
-		.output_clock_frequency1("10.000000 MHz"),
+		.output_clock_frequency1("10.666666 MHz"),
 		.phase_shift1("0 ps"),
 		.duty_cycle1(50),
 		.output_clock_frequency2("0 MHz"),
